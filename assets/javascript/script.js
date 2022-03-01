053f54c6d815a8948439e024ed64087bb9cd0e1b
rlorimier <rere.rnt@gmail.com>
2022-03-01 15:13:18 +0000

    add elements to show the shuffled questions and start to build function to show the answers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,11 @@
 let quizQuestions = [
     {
         question: "What is an Equinox:",
-        answers: {
-            a: "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk",
-            b: "The point where the Sun appears to reach either its highest or lowest point in the sky for the year",
-            c: "The effect caused by the interaction of charged particles from the Sun with atoms in the upper atmosphere"
-        },
-        correctAnswer: "a"
+        answers: [
+            {text: "The time when the plane of Earth's equator passes through the geometric center of the Sun's disk", correct: true},
+            {text: "The point where the Sun appears to reach either its highest or lowest point in the sky for the year", correct: false},
+            {text: "The effect caused by the interaction of charged particles from the Sun with atoms in the upper atmosphere", correct: false}
+        ],
     },
     {
         question: "What NASA stands for:",
@@ -95,16 +94,31 @@ let quizQuestions = [
 let startBtn = document.getElementById("start-btn");
 let questionContainer = document.getElementById("questions");
 
+
+
+let shuffleQuestions, currentQuestion
+
 startBtn.addEventListener("click", startQuiz)
 
 function startQuiz() {
     console.log("hello")
-    startBtn.classList.add('hide')
+    startBtn.classList.add("hide")
+    shuffleQuestions = quizQuestions.sort(() => Math.random() -.5)
+    currentQuestion = 0
     questionContainer.classList.remove("hide")
+    nextQuestion()
 }
 
 function nextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestion])
+}
 
+function showQuestion(question) {
+    questionContainer.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement("label")
+        button.innerText = answer.text
+    }
 }
 
 function selectAnswer() {
