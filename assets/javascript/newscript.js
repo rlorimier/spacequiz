27410f177e545db4a1e253fec0b74d64b3bf9ed7
rlorimier <rere.rnt@gmail.com>
2022-03-09 11:26:23 +0000

    new js file created and main functions added

---
+++ assets/javascript/newscript.js
@@ -0,0 +1,24 @@
+// to start the game
+function startQuiz() {
+
+}
+
+// to show the questions
+function showQuestions() {
+
+}
+
+// to check the users answer
+function checkAnswer() {
+
+}
+
+// to show the correct answer
+function correctAnswer() {
+
+}
+
+// to increment ths score
+function incrementScore() {
+    
+}
